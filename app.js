7a64e1ed05e493711f5b120cac7d24544bbe64be
Yukimasa Shimada <eleos@eleos.jp>
2020-09-08 20:31:03 +0900

    配列とループで書いてみました

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 'use strict';
-const memo = new Map();
-memo.set(0, 0);
-memo.set(1, 1);
-function trib(n) {
-    if (memo.has(n)) {
-        return memo.get(n);
-    }
-    const value = trib(n - 1) + trib(n - 2);
-    memo.set(n, value);
-    return value;
+
+/**
+ * 何ボナッチでも n 番目の数を返す
+ * @param {number} n - 求めたい n 番目の数
+ * @param {array} [0, 1, ...] - 要素数ボナッチを求める。値が初期値。
+ * @return {number} ナントカボナッチの n 番目の数
+ */
+function fib( n , arr = [0, 1]) { 
+        if ( n < 0 ) return false; // マイナスはやらない
+        if ( n < arr.length ) return arr[n]; // 配列の要素にあればその値を返す
+        for (let i = arr.length ; i <= n ; i++) { // 配列の値を配列の合計に置き換えつつループ
+                arr[i % arr.length] =
+                        arr.reduce((p, c) =>  { // このへんで配列の合計を計算しています
+                                return p + c;
+                        });
+        }
+        return arr[n % arr.length];
 }
+
 const length = 40;
 for (let i = 0; i <= length; i++) {
-    console.log(trib(i));
+  console.log(fib(i,[0, 0, 1]));
 }
+
